1b4edb7443d2c8527cbd3d55cce2f6754f2cd1d8
Tomas Jakobsson <tompsson@hotmail.com>
2025-10-25 14:57:49 +0200

    Fix so it also detects if the snippit should be inverted

--- b2sbackglassdesigner/b2sbackglassdesigner/classes/Tab/B2STabPage.vb
+++ b2sbackglassdesigner/b2sbackglassdesigner/classes/Tab/B2STabPage.vb
@@ -1079,23 +1079,30 @@ Public Class B2STabPage
                 .Location = loc
                 .Image = image
                 .IsImageSnippit = True
-                If Char.IsDigit(name.Last()) Then
-                    Dim copy As String = name
-                    Dim numbers As String = ""
-                    While Char.IsDigit(copy.Last())
-                        numbers = copy.Last() + numbers
-                        copy = copy.Substring(0, copy.Length - 1)
+
+                If name.Length AndAlso Char.IsDigit(name.Last()) Then
+                    Dim romtype As String = name
+                    Dim romid As String = ""
+
+                    While romtype.Length AndAlso Char.IsDigit(romtype.Last())
+                        romid = romtype.Last() + romid
+                        romtype = romtype.Substring(0, romtype.Length - 1)
                     End While
-                    If numbers.Length AndAlso copy.Length Then
-                        If copy.Substring(copy.Length - 1, 1) = "L" Then
-                            .RomID = CInt(numbers)
+
+                    If romid.Length Then
+                        .RomID = CInt(romid)
+                    End If
+
+                    If romid.Length AndAlso romtype.Length Then
+                        If romtype.EndsWith("L") Then
                             .RomIDType = eRomIDType.Lamp
-                        ElseIf copy.Substring(copy.Length - 1, 1) = "S" Then
-                            .RomID = CInt(numbers)
+                            .RomInverted = romtype.EndsWith("iL")
+                        ElseIf romtype.EndsWith("S") Then
                             .RomIDType = eRomIDType.Solenoid
-                        ElseIf copy.Length > 1 AndAlso copy.Substring(copy.Length - 2, 2) = "Gi" Then
-                            .RomID = CInt(numbers)
+                            .RomInverted = romtype.EndsWith("iS")
+                        ElseIf romtype.EndsWith("Gi") Then
                             .RomIDType = eRomIDType.GIString
+                            .RomInverted = romtype.EndsWith("iGi")
                         End If
                     End If
                 End If
